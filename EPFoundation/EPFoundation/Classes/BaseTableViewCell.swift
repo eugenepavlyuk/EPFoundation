ffd268f47bdc8530ed2d5bac5179bd7e2011a89a
Ievgen Pavliuk <ievgen.pavliuk@virtual-solution.com>
2018-05-18 17:57:27 +0200

    updated BaseTableViewCell class

--- EPFoundation/EPFoundation/Classes/BaseTableViewCell.swift
+++ EPFoundation/EPFoundation/Classes/BaseTableViewCell.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class BaseTableViewCell: UITableViewCell {
+public class BaseTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
+    public override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    public override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
